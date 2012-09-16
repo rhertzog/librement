02bc9920168737bb56030e2ce50d087ebb0ba8df
Chris Lamb <lamby@debian.org>
2012-09-16 23:57:23 +0100

    Store account type.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/profile/models.py
+++ src/librement/profile/models.py
@@ -1,8 +1,12 @@
+from django.db import models
+
 from django_enumfield import EnumField
 
 from librement.utils.user_data import PerUserData
 
-from .enums import CountryEnum
+from .enums import AccountEnum, CountryEnum
 
 class Profile(PerUserData('profile')):
+    account_type = EnumField(AccountEnum)
+
     country = EnumField(CountryEnum)
