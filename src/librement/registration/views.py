69e2473beabda7de5b22ea0c4cbc6f20ca8a3c2d
Chris Lamb <lamby@debian.org>
2012-09-17 00:10:25 +0100

    Stub registration.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/registration/views.py
+++ src/librement/registration/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from .forms import RegistrationForm
+
 def view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            assert False, user
+    else:
+        form = RegistrationForm()
+
     return render(request, 'registration/view.html', {
+        'form': form,
     })
