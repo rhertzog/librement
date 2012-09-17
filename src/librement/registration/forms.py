2bd6e04fe21658766a5107c4b0d3ed25c8c90275
Chris Lamb <lamby@debian.org>
2012-09-17 23:42:41 +0100

    Group these.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/registration/forms.py
+++ src/librement/registration/forms.py
@@ -68,9 +68,7 @@ class RegistrationForm(forms.ModelForm):
             first_name=self.cleaned_data['first_name'],
             last_name=self.cleaned_data['last_name'],
         )
-
         user.set_password(self.cleaned_data['password'])
-
         user.save()
 
         return user
