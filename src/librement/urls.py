408126458f820cae3d6375d5f020bb7f91165582
Chris Lamb <lamby@debian.org>
2012-09-18 00:50:57 +0100

    Add empty profile views.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/urls.py
+++ src/librement/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     (r'', include('librement.profile.urls', namespace='profile')),
     (r'', include('librement.registration.urls', namespace='registration')),
     (r'', include('librement.passwords.urls', namespace='passwords')),
+    (r'', include('librement.profile.urls', namespace='profile')),
     (r'', include('librement.static.urls', namespace='static')),
 
     (r'', include('librement.debug.urls')),
