909ac7174546aaa9c5d649d71d323746a58d8a05
Chris Lamb <lamby@debian.org>
2012-09-18 00:59:41 +0100

    Add profile edit view.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/profile/urls.py
+++ src/librement/profile/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('librement.profile.views',
     url(r'^people/(?P<username>[^/]+)$', 'view',
         name='view'),
+    url(r'^profile/edit$', 'edit',
+        name='edit'),
 )
--- src/librement/profile/views.py
+++ src/librement/profile/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def view(request, username):
     user = get_object_or_404(User, username=username)
@@ -7,3 +8,7 @@ def view(request, username):
     return render(request, 'profile/view.html', {
         'user': user,
     })
+
+@login_required
+def edit(request):
+    return render(request, 'profile/edit.html')
