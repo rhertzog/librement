e2e83faadf78e9300a21395d036366539983f679
Chris Lamb <lamby@debian.org>
2012-09-18 16:15:40 +0100

    Store media here.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/settings/defaults/__init__.py
+++ src/librement/settings/defaults/__init__.py
@@ -56,7 +56,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/storage/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'swwG4rLmoCMYkKn46r1bOtZLlnUAXFatwY9pv6pzyysYcssHShzse7WSq'
