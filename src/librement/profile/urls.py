f74a63353327c7456317d85e679aed093b1d7a76
Chris Lamb <lamby@debian.org>
2012-09-18 14:50:35 +0100

    Nicer editing of the url.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/profile/urls.py
+++ src/librement/profile/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, url, include
 urlpatterns = patterns('librement.profile.views',
     (r'', include('librement.profile.links.urls', namespace='links')),
 
-    url(r'^people/(?P<username>[^/]+)$', 'view',
+    url(r'^people/(?P<username>[^/]*)$', 'view',
         name='view'),
     url(r'^profile/edit$', 'edit',
         name='edit'),
