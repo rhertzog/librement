d2d67d59c7f5f29832b42bc20d914068c529c609
Chris Lamb <lamby@debian.org>
2012-09-18 01:02:33 +0100

    Simple editing of this field.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/profile/views.py
+++ src/librement/profile/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+
+from .forms import ProfileForm
 
 def view(request, username):
     user = get_object_or_404(User, username=username)
@@ -11,4 +13,17 @@ def view(request, username):
 
 @login_required
 def edit(request):
-    return render(request, 'profile/edit.html')
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user.profile)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('profile:edit')
+
+    else:
+        form = ProfileForm(instance=request.user.profile)
+
+    return render(request, 'profile/edit.html', {
+        'form': form,
+    })
