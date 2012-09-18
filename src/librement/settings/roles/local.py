072478832834db8c9e5749a1da80c95d99435500
Chris Lamb <lamby@debian.org>
2012-09-18 16:17:55 +0100

    Location for storing media.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/settings/roles/local.py
+++ src/librement/settings/roles/local.py
@@ -1,5 +1,11 @@
 from os.path import join, dirname, abspath
 
+def base_dir(*paths):
+    return join(
+        dirname(dirname(dirname(dirname(abspath(__file__))))),
+        *paths
+    )
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -17,6 +23,7 @@ DATABASES = {
 CSRF_COOKIE_DOMAIN = None
 SESSION_COOKIE_DOMAIN = None
 
+MEDIA_ROOT = base_dir('storage')
 STATIC_MEDIA_URL = '/media/%(path)s'
 
 SITE_URL = 'http://127.0.0.1:8000'
