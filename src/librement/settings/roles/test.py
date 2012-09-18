d868409bda5afb597fc0f6fcaa51f32636c65871
Chris Lamb <lamby@debian.org>
2012-09-18 12:29:29 +0100

    Fix tests.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- src/librement/settings/roles/test.py
+++ src/librement/settings/roles/test.py
@@ -1,1 +1,10 @@
 from local import *
+
+# Django's tests require the use of the standard ModelBackend
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
+
+# Don't use bcrypt to run tests
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+)
+
