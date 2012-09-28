2fa44b1f8e6865f1159471d0bad8229882b08e37
Raphaël Hertzog <hertzog@debian.org>
2012-09-28 17:18:27 +0200

    Add copyright and license notices to all Python files
    We're following the recommendations of the Software Freedom Law Center:
    http://softwarefreedom.org/resources/2012/ManagingCopyrightInformation.html
    
    We're acknowledging that up-to-now all the intellectual property
    of Librement is owned by Freexian SARL.

--- src/librement/profile/enums.py
+++ src/librement/profile/enums.py
@@ -1,3 +1,14 @@
+# Copyright 2012 The Librement Developers
+#
+# See the AUTHORS file at the top-level directory of this distribution
+# and at http://librement.net/copyright/
+#
+# This file is part of Librement. It is subject to the license terms in
+# the LICENSE file found in the top-level directory of this distribution
+# and at http://librement.net/license/. No part of Librement, including
+# this file, may be copied, modified, propagated, or distributed except
+# according to the terms contained in the LICENSE file.
+
 from django_enumfield import Item, make_enum
 
 AccountEnum = make_enum('AccountEnum',
